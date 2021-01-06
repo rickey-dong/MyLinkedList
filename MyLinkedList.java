ddde05110c4423af48daf4890d458a2c64532d17
Rickey Dong <rickeyd2004@gmail.com>
2021-01-06 18:18:06 -0500

    created boolean add method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -5,11 +5,28 @@ public class MyLinkedList
   public MyLinkedList()
   {
     size = 0;
-    head = null;
-    tail = null;
   }
   public int size()
   {
     return size;
+  }
+  public boolean add(String value)
+  {
+    if (size == 0)
+    {
+      head.setData(value);
+      tail.setData(value);
+      size++;
+      return true;
+    }
+    else
+    {
+      tail.setData(value);
+      tail.setNext(null);
+      tail.setPrev(tail);
+      tail.getPrev().setNext(tail);
+      size++;
+      return true;
+    }
   }
 }
