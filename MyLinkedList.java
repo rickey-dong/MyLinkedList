fb316fd3fdff37b60833dbf64bf4a555b608f2a7
Rickey Dong <rickeyd2004@gmail.com>
2021-01-06 21:13:00 -0500

    fixed the constructor so that instead of setting head = null, head is now a Node of value null. also fixed the add method since what I did before was that I changed head's value like head.setData(value) which was not good because that deletes a Node and then the LinkedList would be at most of two nodes so I had to add actual nodes using the Node constructor. created the getNthNode method and finished void add

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -5,6 +5,8 @@ public class MyLinkedList
   public MyLinkedList()
   {
     size = 0;
+    head = new Node(null);
+    tail = new Node(null);
   }
   public int size()
   {
@@ -12,21 +14,59 @@ public class MyLinkedList
   }
   public boolean add(String value)
   {
-    if (size == 0)
+    if (size() == 0)
     {
-      head.setData(value);
-      tail.setData(value);
+      Node appendThisNode = new Node(value);
+      head = appendThisNode;
+      tail = appendThisNode;
       size++;
       return true;
     }
     else
     {
-      tail.setData(value);
-      tail.setNext(null);
-      tail.setPrev(tail);
-      tail.getPrev().setNext(tail);
+      Node appendThisNode = new Node(value);
+      appendThisNode.setPrev(tail);
+      tail = appendThisNode;
+      tail.getPrev().setNext(appendThisNode);
       size++;
       return true;
     }
+  }
+  public void add(int index, String value)
+  {
+    if (index < 0 || index > size())
+    {
+      throw new IndexOutOfBoundsException("index " + index + " is out of bounds.");
+    }
+    if (index == size())
+    {
+      add(value);
+    }
+    else if (index == 0)
+    {
+      Node insertThisNode = new Node(value);
+      insertThisNode.setNext(head);
+      head = insertThisNode;
+      head.getNext().setPrev(insertThisNode);
+      size++;
+    }
+    else
+    {
+      Node insertThisNode = new Node(value);
+      insertThisNode.setPrev(getNthNode(index));
+      insertThisNode.setNext(getNthNode(index+1));
+      insertThisNode.getPrev().setNext(insertThisNode);
+      insertThisNode.getNext().setPrev(insertThisNode);
+      size++;
+    }
+  }
+  private Node getNthNode(int n)
+  {
+    Node current = head;
+    for (int i = 0; i < n - 1; i++)
+    {
+      current = current.getNext();
+    }
+    return current;
   }
 }
